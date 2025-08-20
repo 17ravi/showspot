16c047bed1148bec71e5229308d0c4fa1b7a6bae
17ravi <17ravipatel@gmail.com>
2025-08-20 19:50:32 -0400

    Initial Code with structured folders

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,16 @@
+// src/navigation/RootNavigator.tsx
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default RootNavigator;
